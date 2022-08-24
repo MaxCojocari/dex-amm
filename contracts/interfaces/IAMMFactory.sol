6927c912af896c4cc94aa3d03284eb65f52444fe
MaxCojocari <92053176+MaxCojocari@users.noreply.github.com>
2022-08-24 10:51:21 +0300

    Update IAMMFactory.sol

--- contracts/interfaces/IAMMFactory.sol
+++ contracts/interfaces/IAMMFactory.sol
@@ -2,14 +2,31 @@
 pragma solidity ^0.8.9;
 
 interface IAMMFactory {
+    /* 
+    * @dev Adds a new pair of two distinct ERC20 tokens. It reverts if
+    * pair already exists, even though the tokens are swaped.
+    *
+    * @return The address of the pool contract for that pair of tokens.
+    */
     function addPair(address _token0, address _token1)
         external
         returns (address);
 
+
+    /*
+    * @dev Returns the address of the pool contract for specific pair of tokens.
+    * Zero address is returned if pool doesn't exist.
+    *
+    * @return The address of the pool
+    */
     function getAddressPair(address _token0, address _token1)
         external
         view
         returns (address);
 
+
+    /*
+    * @dev This emits if a new pool is created. 
+    */
     event AddPair(address indexed token0, address indexed token1, address pair);
 }
